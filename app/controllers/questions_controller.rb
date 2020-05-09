09dd0c3220d5013a6d0412b5f89043f467566137
Jasmine Dang <ngandth.1992@gmail.com>
2020-05-09 23:56:58 +0800

    Moved coach's reply logic from view back to controller

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -4,5 +4,18 @@ class QuestionsController < ApplicationController
 
   def answer
     @question = params[:question]
+    @answer = ask_coach(@question)
+  end
+
+  private
+
+  def ask_coach(question)
+    if question == 'I am going to work'
+      'Great!'
+    elsif question.end_with?('?')
+      'Silly question, get dressed and go to work!'
+    else
+      'I don\'t care, get dressed and go to work!'
+    end
   end
 end
